a4d550df96dd8bb78147bbe3d2f9e55fd3336312
Lukasz.Cmielowski@pl.ibm.com <Lukasz.Cmielowski@pl.ibm.com>
2019-05-16 14:38:01 +0200

    custom engine for  credit risk azure service endpoint

--- applications/custom-ml-engine-azure/app.py
+++ applications/custom-ml-engine-azure/app.py
@@ -4,66 +4,46 @@ from flask import Flask
 
 app = Flask(__name__, static_url_path='')
 
-import pandas as pd
 import flask
-import os
-from sklearn.externals import joblib
 import requests
 import json
 
 
-credit_model = None
+def convert_user_output_2_openscale(output_data):
+    output_data = json.loads(output_data)
+    users_records = output_data['output']
+    openscale_fields = list(users_records[0])
+    openscale_values = [[rec[k] for k in openscale_fields] for rec in users_records]
+
+    return {'fields': openscale_fields, 'values': openscale_values}
 
 
-def load_credit_model():
-    global credit_model
+def convert_openscale_input_2_user(input_data):
+    openscale_fields = input_data['fields']
+    openscale_values = input_data['values']
 
-    credit_model_path = os.path.join(os.getcwd(), 'models', 'credit', 'german_credit_risk.joblib')
-    credit_model = joblib.load(credit_model_path)
-
-
-load_credit_model()
+    users_records = [{k: v for k, v in zip(openscale_fields, rec)} for rec in openscale_values]
+    return users_records
 
 
 @app.route("/v1/deployments/credit/online", methods=["POST"])
 def credit_online():
-    response = {}
-    labels = ['Risk', 'No Risk']
+
+    # original scoring endpoint with credit risk deployment (Azure ML Service)
+    scoring_endpoint = 'http://20.189.138.213:80/score'
+    scoring_headers = {'Content-Type': 'application/json'}
+    openscale_output = None
 
     if flask.request.method == "POST":
         payload = flask.request.get_json()
 
         if payload is not None:
-            df = pd.DataFrame.from_records(payload['values'], columns=payload['fields'])
-            scores = credit_model['model'].predict_proba(df).tolist()
-            predictions = credit_model['postprocessing'](credit_model['model'].predict(df))
-            response = {'fields': ['prediction', 'probability'], 'labels': labels,
-                        'values': list(map(list, list(zip(predictions, scores))))}
+            user_input = convert_openscale_input_2_user(payload)
+            response = requests.post(scoring_endpoint, json=user_input, headers=scoring_headers)
+            output_data = response.json()
+            openscale_output = convert_user_output_2_openscale(output_data)
 
-    return flask.jsonify(response)
-
-
-@app.route("/v1/deployments/circle/online", methods=["POST"])
-def circle_online():
-    response = {}
-    scoring_url = 'http://13.86.235.42:80/score'
-    prediction_field = 'area'
-    predictions = []
-
-    if flask.request.method == "POST":
-        payload = flask.request.get_json()
-
-        if payload is not None:
-            values = payload['values']
-            fields = payload['fields']
-
-            for value in values:
-                result = json.loads(requests.post(scoring_url, json={fields[0]: value[0]}).json())
-                predictions.append([result[prediction_field]])
-
-            response = {'fields': [prediction_field], 'values': predictions}
-
-    return flask.jsonify(response)
+    return flask.jsonify(openscale_output)
 
 
 @app.route("/v1/deployments", methods=["GET"])
@@ -73,7 +53,7 @@ def get_deployments():
     if flask.request.method == "GET":
         host_url = flask.request.host
         response = {
-            "count": 2,
+            "count": 1,
             "resources": [
                 {
                     "metadata": {
@@ -82,7 +62,7 @@ def get_deployments():
                         "modified_at": "2019-01-02T12:00:22Z"
                     },
                     "entity": {
-                        "name": "German credit risk compliant deployment",
+                        "name": "German credit risk perturbed scoring endpoint",
                         "description": "Scikit-learn credit risk model deployment",
                         "scoring_url": "https://{}/v1/deployments/credit/online".format(host_url),
                         "asset": {
@@ -92,26 +72,6 @@ def get_deployments():
                         "asset_properties": {
                                "problem_type": "binary",
                                "input_data_type": "structured",
-                        }
-                    }
-                },
-                {
-                    "metadata": {
-                        "guid": "circle",
-                        "created_at": "2019-01-01T10:11:12Z",
-                        "modified_at": "2019-01-02T12:00:22Z"
-                    },
-                    "entity": {
-                        "name": "Circle model deployment",
-                        "description": "Azure ML service circle surface prediction deployment",
-                        "scoring_url": "https://{}/v1/deployments/circle/online".format(host_url),
-                        "asset": {
-                            "name": "circle",
-                            "guid": "circle"
-                        },
-                        "asset_properties": {
-                            "problem_type": "regression",
-                            "input_data_type": "structured",
                         }
                     }
                 }

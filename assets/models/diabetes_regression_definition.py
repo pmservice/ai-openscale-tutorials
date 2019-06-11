746c01f4a08f6ed631a4f965fc57d2932a3c6734
Wojciech Sobala <wojciech.sobala@pl.ibm.com>
2019-06-11 11:46:16 +0200

    diabetes datasets + definition

--- assets/models/diabetes_regression_definition.py
+++ assets/models/diabetes_regression_definition.py
@@ -1,15 +1,14 @@
 from sklearn import datasets, linear_model
 from sklearn.metrics import mean_squared_error, r2_score
 from joblib import dump
+import pandas as pd
 
-diabetes_data = datasets.load_diabetes()
-diabetes_X = diabetes_data.data
-diabetes_y = diabetes_data.target
-
-diabetes_X_train = diabetes_X[:-20]
-diabetes_y_train = diabetes_y[:-20]
-diabetes_X_test = diabetes_X[-20:]
-diabetes_y_test = diabetes_y[-20:]
+diabetes_train = pd.read_csv('diabetes_train.csv', index_col=None)
+diabetes_X_train = diabetes_train.iloc[:, 0:-1]
+diabetes_y_train = diabetes_train[['y']]
+diabetes_test = pd.read_csv('diabetes_feedback.csv', index_col=None)
+diabetes_X_test = diabetes_test.iloc[:, 0:-1]
+diabetes_y_test = diabetes_test[['y']]
 
 model = linear_model.LinearRegression()
 model.fit(diabetes_X_train, diabetes_y_train)
@@ -17,6 +16,7 @@ model.fit(diabetes_X_train, diabetes_y_train)
 print("R-squared on train set: {:.2f}".format(r2_score(diabetes_y_train, model.predict(diabetes_X_train))))
 print("R-squared on test set: {:.2f}".format(r2_score(diabetes_y_test, model.predict(diabetes_X_test))))
 
-
 model_path="diabetes_regression.joblib"
 dump(model, model_path)
+
+print("Model has been stored to file: {}".format(model_path))
